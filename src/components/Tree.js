11b3ab3f030c569f1d2cb974347d72eaa2d06a9d
Ionut Achim <achimvionut@gmail.com>
2020-05-25 13:03:25 +0300

    feat: `displayPropertiesMaxCount` option
    limit the number of displayed entries for objects and add an ellipsis to display the rest

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import { useRecoilValue } from 'recoil'
 
 import { themeState } from '../theme'
+import { optionsState } from '../options'
 import { Entry } from './Entry'
 
 const style = {
@@ -12,6 +13,17 @@ const style = {
 
 export function Tree({ data, depth, ...props }) {
   const { fontSize, tabWidth } = useRecoilValue(themeState)
+  const { displayPropertiesMaxCount } = useRecoilValue(optionsState)
+  const [truncate, toggleTruncate] = useState(true)
+
+  const entries = useMemo(() => {
+    const list = Object.entries(data ?? {})
+    return truncate && list.length > displayPropertiesMaxCount
+      ? list.slice(0, displayPropertiesMaxCount)
+      : list
+  }, [data, truncate, displayPropertiesMaxCount])
+
+  const size = Object.keys(data ?? {}).length ?? 0
 
   return (
     <ol
@@ -22,9 +34,19 @@ export function Tree({ data, depth, ...props }) {
         ...props.style,
       }}
     >
-      {Object.entries(data ?? {}).map(([k, v]) => (
+      {entries.map(([k, v]) => (
         <Entry key={k} data={[k, v]} depth={depth} />
       ))}
+      {truncate && size > displayPropertiesMaxCount && (
+        <span
+          style={{ cursor: 'pointer' }}
+          onClick={() => toggleTruncate(false)}
+        >
+          {'('}
+          <ins>{size - displayPropertiesMaxCount} more...</ins>
+          {')'}
+        </span>
+      )}
     </ol>
   )
 }
