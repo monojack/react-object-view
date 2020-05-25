c6248a5405c60b69072302998621879b02fb1a5c
Ionut Achim <achimvionut@gmail.com>
2020-05-25 11:22:24 +0300

    feat: implement `customEntry`
    same as `customValue` but will render in place of the entire key/value entry

--- src/symbols.js
+++ src/symbols.js
@@ -6,6 +6,10 @@ export const mapEntry = (() =>
   (typeof Symbol === 'function' && Symbol.ROVMapEntry) ||
   Symbol('ROVMapEntry'))()
 
+export const customEntry = (() =>
+  (typeof Symbol === 'function' && Symbol.ROVCustomEntry) ||
+  Symbol('ROVCustomEntry'))()
+
 export const customValue = (() =>
   (typeof Symbol === 'function' && Symbol.ROVCustomValue) ||
   Symbol('ROVCustomValue'))()
